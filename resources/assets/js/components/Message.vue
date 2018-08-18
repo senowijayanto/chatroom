266edd40262781c8dfa5ba317e2b0568ebd02460
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-18 22:02:55 +0700

    Create Message Component and Vuejs Chat Scroll

---
+++ resources/assets/js/components/Message.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div class="">
+      <li class="list-group-item" :class="className"><slot></slot></li>
+      <small class="badge float-right" :class="badgeColor">You</small>
+    </div>
+</template>
+
+<script>
+export default {
+  props: [
+    'color'
+  ],
+  computed: {
+    className() {
+      return 'list-group-item-' + this.color
+    },
+    badgeColor() {
+      return 'badge-'+this.color
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -5,18 +5,33 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Chat</title>
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <style media="screen">
+      .list-group {
+        overflow-y: scroll;
+        height: 200px;
+      }
+    </style>
   </head>
   <body>
     <div class="container">
       <div class="row" id="app">
-        <ul class="list-group offset-4 col-4">
+        <div class="offset-4 col-4">
           <li class="list-group-item active">Chat Room</li>
-          <li class="list-group-item">Dapibus ac facilisis in</li>
-          <li class="list-group-item">Morbi leo risus</li>
-          <li class="list-group-item">Porta ac consectetur ac</li>
-          <li class="list-group-item">Vestibulum at eros</li>
-          <input type="text" class="form-control" placeholder="Type your message here...">
-        </ul>
+          <ul class="list-group" v-chat-scroll>
+            <Message
+              v-for="(value, index) in chat.message"
+              :key="index"
+              color="info">
+              @{{value}}
+            </Message>
+          </ul>
+          <input type="text"
+          class="form-control"
+          placeholder="Type your message here..."
+          v-model="message"
+          @keyup.enter="send"
+          >
+        </div>
       </div>
     </div>
 
