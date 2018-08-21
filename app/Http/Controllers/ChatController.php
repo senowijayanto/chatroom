ecd6045d00695e2178cc98be2b314f519ceeb84a
Seno Wijayanto <senowijayanto@gmail.com>
2018-08-21 23:11:58 +0700

    Event to Broadcast

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ChatController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
+    public function chat()
+    {
+      return view('chat');
+    }
+
+    public function send(request $request)
+    {
+      $user = User::find(Auth::id());
+      event(new ChatEvent($request->message, $user));
+    }
+}
